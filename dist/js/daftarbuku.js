2bba98f54a634714926ab29612893d08ff73d2c6
Alfian Aulia Firdaus <alfian@kompiscreative.tech>
2021-03-19 04:42:33 +0700

    fixing session

--- dist/js/daftarbuku.js
+++ dist/js/daftarbuku.js
@@ -1,15 +1,5 @@
 fetchdata();
 tekan();
-checkSession();
-
-function checkSession(){
-    const storedSession = localStorage.getItem("session");
-  if(storedSession===null){
-    window.location = "../login.html";
-  } else{
-    console.log(storedSession);
-  }
-}
 
 
 function fetchdata() {
